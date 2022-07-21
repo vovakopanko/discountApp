4bca1b73e872259d88767d87c382d6e63f269dfa
Vladzimir <58780728+vovakopanko@users.noreply.github.com>
2022-07-21 17:09:09 +0300

    add tsconfig

--- App.tsx
+++ App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
 export default function App() {
   return (
