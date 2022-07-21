b5d5a9205137725102acf6fd7f2d26ed5228d959
Vladzimir <58780728+vovakopanko@users.noreply.github.com>
2022-07-21 17:50:55 +0300

    add redux

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View, StyleSheet } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import reduxStore from "./src/redux/reduxStore";
 
 export default function App() {
+  const { store, persistor } = reduxStore();
   return (
-    <View style={styles.container}>
-      <Text>Demo Discount App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+          <View style={styles.container}>
+            <Text>Demo Discount App</Text>
+            <StatusBar style="auto" />
+          </View>
+      </PersistGate>
+    </Provider>
   );
 }
 
